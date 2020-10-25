7ac9755c71c0145bb71e127be07231c3125a98c7
FlorianSL <fsaintlezer@gmail.com>
2020-10-25 19:29:57 +0100

    variables, bomb class attribution and making box number and class match

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,9 @@ const container = document.getElementById("gridContainer");
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
+  for (c = 1; c < (rows * cols) + 1; c++) { //  Changé depuis for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
+    cell.innerText = (c); //changé depuis cell.innerText = (c+1); 
     container.appendChild(cell).className = "grid-item case"+c+"";
   };
 };
@@ -24,3 +24,17 @@ $( "div" )
     $( this ).css("background-color","white");
   });
    
+
+// Variables 
+var totalTiles = 10 * 10;
+var totalBomb = Math.round(totalTiles / 10);
+var flag = 0; 
+var counter = 1;
+
+
+// Attribution aléatoire de la classe 'bomb'
+for(var bomb=0; bomb < totalBomb; bomb++) {
+var bombTile = Math.floor(Math.random() * totalTiles) + 1;
+console.log('Bombe à la case '+bombTile);
+$('.case'+bombTile).addClass('bomb');
+}
